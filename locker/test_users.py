caa0fe3ffdf9b884133ce21888fdddad68846f52
yontey <muasajohn01@gmail.com>
2019-04-05 18:08:11 +0300

    added delete test

---
+++ locker/test_users.py
@@ -0,0 +1,52 @@
+import unittest
+import users  
+
+class TestUsers(unittest.TestCase):
+    '''
+    TestCase that defines test cases for the user and credential class behaviours
+    '''
+    def setUp(self):
+        '''
+        setUp method to run before each cases
+        '''
+        self.new_account = users.Users("John","Facebook")
+        self.new_password = users.Credentials("jklmn25")
+    
+    def test_init(self):
+        self.assertEqual(self.new_account.username,"John")
+        self.assertEqual(self.new_account.account,"Facebook")
+        self.assertEqual(self.new_password.password,"jklmn25")
+    
+    def tearDown(self):
+        '''
+        tearDown method that cleans up after each testCase is done
+        '''
+        users.Users.accounts_list = []
+        
+        
+    def test_save_user(self):
+         '''
+         To test if the account created is stored in the users list
+         '''
+         
+         self.new_account.save_user()   
+         self.new_password.save_password()
+         self.assertEqual(len(users.Users.accounts_list),2)
+    
+    def test_save_multiple_accounts(self):
+        '''
+        This is to check whether multiple users can create multiple accounts
+        '''
+        self.new_account.save_user()
+        self.new_password.save_password()
+        
+        test_user = users.Users("test","account")
+        test_password = users.Credentials("abcdefk1")
+        test_user.save_user()
+        test_password.save_password()
+        self.assertEqual(len(users.Users.accounts_list),4)
+    
+    def test_delete_account      
+if __name__ == '__main__':
+    unittest.main()        
+  
--- locker/users.py
+++ locker/users.py
@@ -2,28 +2,27 @@ class Users:
     """
     Class that generate new instances of users
     """
-    
-    accounts_list = [] # empty users list where all accounts created will be stored
+    accounts_list = []# empty users list where all accounts created will be stored
     
     def __init__(self,username,account):
          """
          Args:
          username: new User username
          account: new User account
-         
          """
          self.username = username
          self.account = account
          
-    
-    
-    
-    
-    
+    def save_user(self):
+        Users.accounts_list.append(self)
     
 class Credentials:
     """
     Class that generates new instances of passwords
+     """
+    def __init__(self,password):
+        self.password = password
     
-    """
-    
+    def save_password(self):
+        Users.accounts_list.append(self)
+   
