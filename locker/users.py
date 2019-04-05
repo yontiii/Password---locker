e9d4bca870e105a375588832940370f102e51ba2
yontey <muasajohn01@gmail.com>
2019-04-05 17:45:38 +0300

    added initial test cases

---
+++ locker/users.py
@@ -0,0 +1,29 @@
+class Users:
+    """
+    Class that generate new instances of users
+    """
+    
+    accounts_list = [] # empty users list where all accounts created will be stored
+    
+    def __init__(self,username,account):
+         """
+         Args:
+         username: new User username
+         account: new User account
+         
+         """
+         self.username = username
+         self.account = account
+         
+    
+    
+    
+    
+    
+    
+class Credentials:
+    """
+    Class that generates new instances of passwords
+    
+    """
+    
